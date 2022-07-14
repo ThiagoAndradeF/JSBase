a0129339f62f97ac4329bb752a07d0c39cfe2b8f
Thiago Andrade <90008244+ThiagoAndradeF@users.noreply.github.com>
2022-07-14 16:02:52 -0300

    .

---
+++ JS_Array/15_concat.js
@@ -0,0 +1,8 @@
+const filhas = ["Ualeskah" , "Cibalaena"]
+const filhos = ["Clovis" , "Geribaldo"]
+
+const parentes= filhas.concat(filhos)
+console.log(parentes)
+
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
