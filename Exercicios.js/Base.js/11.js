af0c01afa8d983d3b5cd1291f00d675fc9a4b7be
Thiago Andrade <90008244+ThiagoAndradeF@users.noreply.github.com>
2022-07-18 10:15:42 -0300

    .

--- Exercicios.js/Base.js/11.js
+++ Exercicios.js/Base.js/11.js
@@ -4,7 +4,8 @@ const receiveFirstAndLastElement=function(elementos){
     const firstElement= elementos[indicateFirst]
     const lastElement= elementos[indicateLast]
 
-    return "The first element is: "+ firstElement +"\nThe last element is "+ lastElement 
+    
+    return [firstElement,lastElement]
 }
 
 console.log(receiveFirstAndLastElement([10,20,30,40,50,60]))
